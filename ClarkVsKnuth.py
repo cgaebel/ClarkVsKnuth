41d9aff91177e9d0f958a4267e848faf6c951b1b
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-02-04 17:45:56 -0500

    Added the full profiling and actual output.

--- ClarkVsKnuth.py
+++ ClarkVsKnuth.py
@@ -7,12 +7,16 @@
 # that would be asymptotically faster! [log_2(n) vs. 2log_3(n)]
 
 # This script is what I used to prove I'm an idiot.
-# Note to self: Do the math next time.
+# Note to self: Do the math next time - and realize how to change log bases.
 
 import time
 import math
 
+mults = 0
+
 def LinearPower(base, exponent):
+    global mults
+    
     result = base
 
     if exponent == 0:
@@ -21,11 +25,15 @@ def LinearPower(base, exponent):
     for i in range(1, exponent):
         result *= base
 
+    mults += exponent - 1
+
     return result
 
 # Calculates base^exponent, with an interesting running
 # time of: (logBase-1)log_logBase(exponent)
 def Exponentiate(base, exponent, logBase):
+    global mults
+    
     if exponent < 2:
         if exponent == 1:
             return base
@@ -35,6 +43,43 @@ def Exponentiate(base, exponent, logBase):
     if (exponent % logBase) == 0:
         return LinearPower(Exponentiate(base, exponent/logBase, logBase), logBase)
     else:
+        mults += 1
         return Exponentiate(base, exponent - 1, logBase) * base
 
-####
+# returns who won the round. Negative if I won, positive if Knuth won.
+def RunComparisons(exponent):
+    global mults
+    
+    mults = 0
+    Exponentiate(2, exponent, 2)
+    knuthsResult = mults
+    #print("[2^", exponent, " - Knuth]: ", knuthsResult, sep='')
+
+    mults = 0
+    Exponentiate(2, exponent, 3)
+    clarksResult = mults
+    #print("[2^", exponent, " - Clark]: ", clarksResult, sep
+    return knuthsResult - clarksResult
+
+myTotalWins = 0
+clarksWins = 0
+knuthsWins = 0
+ties = 0
+
+iterations = 10000 # Adjust this in relation to your paranoia level.
+
+# This loop checks for all values of n up to -iterations- to see which algorithm is better.
+# If, at any value of n my algorithm works better than knuths, my total wins get incremented.
+for i in range(iterations):
+    result = RunComparisons(i)
+    if result < 0:
+        knuthsWins += 1
+    elif result > 0:
+        clarksWins += 1
+    else:
+        ties += 1
+
+    if clarksWins > knuthsWins:
+        myTotalWins += 1
+
+print("Total wins: ", myTotalWins, "/", iterations, ".", " (", (myTotalWins/iterations) * 100, "%)", sep='')
